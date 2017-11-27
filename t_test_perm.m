ea7b57500e7ddb76ed5e1a76bf9e747353e89e71
Cécile <cecile.issard@etu.parisdescartes.fr>
2017-11-27 09:20:00 +0100

    modification of the structue of the data matrix to be more flexible

--- t_test_perm.m
+++ t_test_perm.m
@@ -1,9 +1,9 @@
-function [t_values] = t_test(donneesoxy,nbabies,nt,nch,conditions);
+function [t_values] = t_test(donneesoxy,nbabies,nt,nch,conditions,seuil);
 
 P = [];
 for bb=1:nbabies;
-    donnees = [donneesoxy(bb,1,:,:) donneesoxy(bb,2,:,:) donneesoxy(bb,3,:,:)];
-    perm_index = randperm(3);
+    donnees = [donneesoxy(bb,1,:,:) donneesoxy(bb,2,:,:) donneesoxy(bb,3,:,:) donneesoxy(bb,4,:,:)];
+    perm_index = randperm(4);
     perm = donnees(:,perm_index,:,:);
     P=cat(1,P,perm); %tableau de données avec les conditions permutées
 end
@@ -14,15 +14,30 @@ seuil=2.045; %définition libre, en fonction de la distribution du t ou pas.
 t_values=zeros(nch,nt);
 for ch=1:nch
     for t=1:nt
-        if strcmp(conditions,'AN')
-            cond1 = P(:,1,t,ch);
+        if strcmp(conditions,'AN') || strcmp(conditions,'NA')
+            cond1 = P(:,3,t,ch);
             cond2 = P(:,2,t,ch);
-        elseif strcmp(conditions,'AB')
-            cond1 = P(:,1,t,ch);
-            cond2 = P(:,3,t,ch);
-        elseif strcmp(conditions,'NB')
+        elseif strcmp(conditions,'AB') || strcmp(conditions,'BA')
+            cond1 = P(:,3,t,ch);
+            cond2 = P(:,1,t,ch);
+        elseif strcmp(conditions,'NB') || strcmp(conditions,'BN')
+            cond1 = P(:,2,t,ch);
+            cond2 = P(:,1,t,ch); 
+        elseif strcmp(conditions,'CB') || strcmp(conditions,'BC')
+            cond1 = P(:,3,t,ch); 
+            cond2 = P(:,1,t,ch);
+        elseif strcmp(conditions,'DB') || strcmp(conditions,'BD')
+            cond1 = P(:,4,t,ch);
+            cond2 = P(:,1,t,ch);
+        elseif strcmp(conditions,'NC') || strcmp(conditions,'CN')
             cond1 = P(:,2,t,ch);
             cond2 = P(:,3,t,ch);
+        elseif strcmp(conditions,'ND') || strcmp(conditions,'DN')
+            cond1 = P(:,2,t,ch);
+            cond2 = P(:,4,t,ch);
+        elseif strcmp(conditions,'CD') || strcmp(conditions,'DC')
+            cond1 = P(:,3,t,ch);
+            cond2 = P(:,4,t,ch);
         end
         [h, p, ci, stats] = ttest(cond1,cond2);
         t_values(ch,t) = stats(1).tstat;
